ee1fc41a47ecf4b922c7e37e437b4f74fe75c3eb
Facundo <75026725+facundoaguilera@users.noreply.github.com>
2022-04-07 23:30:56 -0300

    Update server.sh

--- api/server.sh
+++ api/server.sh
@@ -4,4 +4,6 @@ DJANGODIR=$(cd `dirname $0` && pwd)
 DJANGO_SETTINGS_MODULE=EnsolversDjango.settings
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+xdg-open http://localhost:8000/
+start http://localhost:8000/
 python manage.py runserver 
